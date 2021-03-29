03cfccd01c79ecaab1d1da27cca3e8109914e120
unknown <edina.kurtic90@hotmail.com>
2021-03-29 10:56:02 +0200

    Added day 20-21

---
+++ ex01/src/index.js
@@ -0,0 +1,218 @@
+var textOutput = ""
+var keyboardOutput = document.getElementById("keyboardOutput");
+
+var btn1 = document.getElementById("bat1");
+btn1.onclick = function() {
+    textOutput += "1";
+    keyboardOutput.value = textOutput;
+}
+
+var btn2 = document.getElementById("bat2")
+btn2.onclick = function () {
+    textOutput += "2";
+    keyboardOutput.value = textOutput;
+}
+var btn3 = document.getElementById("bat3");
+btn3.onclick = function () {
+    textOutput += "3";
+    keyboardOutput.value = textOutput;
+}
+var btn4 = document.getElementById("bat4");
+btn4.onclick = function () {
+    textOutput += "4";
+    keyboardOutput.value = textOutput;
+}
+var btn5 = document.getElementById("bat5");
+btn5.onclick = function () {
+    textOutput += "5";
+    keyboardOutput.value = textOutput;
+}
+var btn6 = document.getElementById("bat6");
+btn6.onclick = function () {
+    textOutput += "6";
+    keyboardOutput.value = textOutput;
+}
+var btn7 = document.getElementById("bat7");
+btn7.onclick = function () {
+    textOutput += "7";
+    keyboardOutput.value = textOutput;
+}
+
+var btn8 = document.getElementById("bat8");
+btn8.onclick = function () {
+    textOutput += "8";
+    keyboardOutput.value = textOutput;
+}
+var btn9 = document.getElementById("bat9");
+btn9.onclick = function () {
+    textOutput += "9";
+    keyboardOutput.value = textOutput;
+}
+
+var btn10 = document.getElementById("bat0");
+btn10.onclick = function () {
+    textOutput += "0";
+    keyboardOutput.value = textOutput;
+}
+var btn11 = document.getElementById("batq");
+btn11.onclick = function () {
+    textOutput += "q";
+    keyboardOutput.value = textOutput;
+}
+var btn12 = document.getElementById("batw");
+btn12.onclick = function () {
+    textOutput += "w";
+    keyboardOutput.value = textOutput;
+}
+var btn13 = document.getElementById("bate");
+btn13.onclick = function () {
+    textOutput += "e";
+    keyboardOutput.value = textOutput;
+}
+var btn14 = document.getElementById("batr");
+btn14.onclick = function () {
+    textOutput += "r";
+    keyboardOutput.value = textOutput;
+}
+var btn15 = document.getElementById("batt");
+btn15.onclick = function () {
+    textOutput += "t";
+    keyboardOutput.value = textOutput;
+}
+var btn16 = document.getElementById("baty");
+btn16.onclick = function () {
+    textOutput += "y";
+    keyboardOutput.value = textOutput;
+}
+var btn17 = document.getElementById("batu");
+btn17.onclick = function () {
+    textOutput += "u";
+    keyboardOutput.value = textOutput;
+}
+var btn18 = document.getElementById("bati");
+btn18.onclick = function () {
+    textOutput += "i";
+    keyboardOutput.value = textOutput;
+}
+var btn19 = document.getElementById("bato");
+btn19.onclick = function () {
+    textOutput += "o";
+    keyboardOutput.value = textOutput;
+}
+var btn20 = document.getElementById("batp");
+btn20.onclick = function () {
+    textOutput += "p";
+    keyboardOutput.value = textOutput;
+}
+var btn21 = document.getElementById("bata");
+btn21.onclick = function () {
+    textOutput += "a";
+    keyboardOutput.value = textOutput;
+}
+var btn22 = document.getElementById("bats");
+btn22.onclick = function () {
+    textOutput += "s";
+    keyboardOutput.value = textOutput;
+}
+var btn23 = document.getElementById("batd");
+btn23.onclick = function () {
+    textOutput += "d";
+    keyboardOutput.value = textOutput;
+}
+var btn24 = document.getElementById("batf");
+btn24.onclick = function () {
+    textOutput += "f";
+    keyboardOutput.value = textOutput;
+}
+var btn25 = document.getElementById("batg");
+btn25.onclick = function () {
+    textOutput += "g";
+    keyboardOutput.value = textOutput;
+}
+var btn26 = document.getElementById("bath");
+btn26.onclick = function () {
+    textOutput += "h";
+    keyboardOutput.value = textOutput;
+}
+var btn27 = document.getElementById("batj");
+btn27.onclick = function () {
+    textOutput += "j";
+    keyboardOutput.value = textOutput;
+}
+var btn28 = document.getElementById("batk");
+btn28.onclick = function () {
+    textOutput += "k";
+    keyboardOutput.value = textOutput;
+}
+var btn29 = document.getElementById("batl");
+btn29.onclick = function () {
+    textOutput += "l";
+    keyboardOutput.value = textOutput;
+}
+var btn30 = document.getElementById("batz");
+btn30.onclick = function () {
+    textOutput += "z";
+    keyboardOutput.value = textOutput;
+}
+
+var btn31 = document.getElementById("batx");
+btn31.onclick = function () {
+    textOutput += "x";
+    keyboardOutput.value = textOutput;
+}
+
+var btn32 = document.getElementById("batc");
+btn32.onclick = function () {
+    textOutput += "c";
+    keyboardOutput.value = textOutput;
+}
+
+var btn33 = document.getElementById("batv");
+btn33.onclick = function () {
+    textOutput += "v";
+    keyboardOutput.value = textOutput;
+}
+
+var btn34 = document.getElementById("batb");
+btn34.onclick = function () {
+    textOutput += "b";
+    keyboardOutput.value = textOutput;
+}
+
+var btn35 = document.getElementById("batn");
+btn35.onclick = function () {
+    textOutput += "n";
+    keyboardOutput.value = textOutput;
+}
+
+var btn36 = document.getElementById("batm");
+btn36.onclick = function () {
+    textOutput += "m";
+    keyboardOutput.value = textOutput;
+}
+
+var btn37 = document.getElementById("bat-");
+btn37.onclick = function () {
+    textOutput += "-";
+    keyboardOutput.value = textOutput;
+}
+
+var btn38 = document.getElementById("bat_");
+btn38.onclick = function () {
+    textOutput += "_";
+    keyboardOutput.value = textOutput;
+}
+
+var btn39 = document.getElementById("batSpace");
+btn39.onclick = function () {
+    textOutput += " ";
+    keyboardOutput.value = textOutput;
+}
+
+var btn40 = document.getElementById("batEnter");
+btn40.onclick = function () {
+    textOutput += "/n";
+    keyboardOutput.value = textOutput;
+}
+
+
